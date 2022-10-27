e44e78d41a949c8eb87eb24c8c9cca152bd54f97
Nicholas <62774419+eggynick@users.noreply.github.com>
2022-10-27 11:19:57 -0400

    Add boiler plate

---
+++ ImportARGOS.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 27 11:14:31 2022
+
+@author: nbf9
+"""
+
+##---------------------------------------------------------------------
+## ImportARGOS.py
+##
+## Description: Read in ARGOS formatted tracking data and create a line
+##    feature class from the [filtered] tracking points
+##
+## Usage: ImportArgos <ARGOS folder> <Output feature class> 
+##
+## Created: Fall 2022
+## Author: nicholas.fairbairn@duke.edu (for ENV859)
+##---------------------------------------------------------------------
